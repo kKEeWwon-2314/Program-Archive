9823ec62ef9cc7780306e0d917bd9c7027c7aa59
Kevin <77865332+kKEeWwon-2314@users.noreply.github.com>
2021-10-31 11:49:50 -0600

    Create solution.py

---
+++ Coding Questions/Simplify Unix Path/solution.py
@@ -0,0 +1,27 @@
+
+path = '/a/./../b//'
+
+s =  [i for x in path.split('/') for i in (x, '/')]
+s.pop()
+s = [x for x in s if x]
+
+while s[-1] == '/':
+    try:
+        s.pop()
+    except ValueError:
+        break
+
+if '.' in s:
+    s.pop(s.index('.') - 1)
+    s.pop(s.index('.'))
+
+while ".." in path:
+    try:
+        s.pop(s.index('..') + 1)
+        s.pop(s.index('..') - 2)
+        s.pop(s.index('..') - 1)
+        s.pop(s.index('..'))
+    except ValueError:
+        break
+
+print(''.join(s))
