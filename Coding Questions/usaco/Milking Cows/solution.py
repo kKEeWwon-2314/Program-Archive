70c8c0d2c519f28ace97b3f123ff396d8263e325
Kevin <77865332+kKEeWwon-2314@users.noreply.github.com>
2021-11-06 16:22:45 -0600

    Create solution.py

---
+++ Coding Questions/usaco/Milking Cows/solution.py
@@ -0,0 +1,33 @@
+start = 0
+end = 0
+
+times = []
+milk_time = []
+idle_time = []
+
+for i in range(int(input())):
+    times.append(list(map(int, input().split())))
+
+times = sorted(times)
+
+start = times[0][0]
+end = times[0][1]
+
+for i in range(len(times)):
+    if (times[i][0] < end):
+        if (times[i][1] < end):
+            pass
+        else:
+            end = times[i][1]
+            milk_time.append(end - start)
+    else:
+        start = times[i][0]
+
+        idle_time.append(start - end)
+        
+        end = times[i][1]
+        
+        milk_time.append(start - end)
+
+print('\n')
+print(max(milk_time), max(idle_time))
